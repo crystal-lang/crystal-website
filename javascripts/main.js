9fd9cc52ac65c3b3ee57dd2458d97d45f4939f14
Valeria Tiffenberg <vtiffenberg@manas.com.ar>
2017-02-03 14:57:40 -0300

    Added jekyll-assets for #12 with suggestions from #16

--- javascripts/main.js
+++
@@ -1,5 +0,0 @@
-var canvas = document.getElementById("logo-canvas")
-var model = new Viewer3D(canvas);
-model.shader("flat", 255, 255, 255);
-model.insertModel("/javascripts/polyhedron/icosahedron.xml");
-model.contrast(0.90);
