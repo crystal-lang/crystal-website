4128b2a2744a34a6c2f1233646ef403e2fd7618f
Martin Scebba <mscebba@manas.com.ar>
2017-03-07 17:57:07 -0300

    Fixing top navbar when scrolling

--- _assets/js/bundle.js
+++ _assets/js/bundle.js
@@ -7,3 +7,20 @@
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
 })
+
+$(document).on('click', 'a', function(event){
+  event.preventDefault();
+  $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top - 100
+  }, 500);
+});
+
+$(document).scroll(function(e){
+    var scrollTop = $(document).scrollTop();
+    if(scrollTop > 620){
+        //console.log(scrollTop);
+        $('.wrapper.main').addClass("fixed");
+    } else {
+        $('.wrapper.main').removeClass("fixed");
+    }
+});
