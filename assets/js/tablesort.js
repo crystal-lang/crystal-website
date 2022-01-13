45e818692425e27952f7f68a8ba7d21efdeb611b
Johannes Müller <straightshoota@gmail.com>
2022-01-13 21:06:14 +0100

    Move tablesort to `assets/js`

---
+++ assets/js/tablesort.js
@@ -0,0 +1,7 @@
+---
+layout: none
+---
+
+{% include_relative _vendor/tablesort-5.2.1/tablesort.js %}
+{% include_relative _vendor/tablesort-5.2.1/sorts/tablesort.number.js %}
+{% include_relative _vendor/tablesort-5.2.1/sorts/tablesort.date.js %}
