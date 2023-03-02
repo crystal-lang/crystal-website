63f49ce052b08d0eadf9cfb7af5fa8d3442e040f
Johannes Müller <straightshoota@gmail.com>
2023-03-02 12:14:33 +0100

    Implement basic site in new design

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -2,23 +2,5 @@
 layout: none
 ---
 
-{% include_relative _materialize.min.js %}
 {% include_relative _viewer.js %}
-{% include_relative _delaunay.js %}
 {% include_relative _main.js %}
-{% include_relative _pagination_in_place.js %}
-
-$( document ).ready(function(){
-  $('.button-collapse').sideNav({
-      closeOnClick: true
-    }
-  );
-
-  $("#nav-mobile a").click(function(e) {
-    e.preventDefault();
-    $("#nav-mobile, #sidenav-overlay, .drag-target").remove();
-    window.setTimeout(function() {
-      window.location = $(e.target).attr("href");
-    }, 0);
-  });
-});
