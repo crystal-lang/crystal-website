0c5922d69a7bb12b3b1262ff7ed2e1db3b91b1d7
Johannes Müller <straightshoota@gmail.com>
2023-03-08 21:28:06 +0100

    Run bundler when entering shell

--- devenv.nix
+++ devenv.nix
@@ -5,6 +5,11 @@
 
   processes.serve.exec = "make serve";
 
+  enterShell = ''
+    # Automatically run bundler upon enterting the shell.
+    bundle
+  '';
+
   pre-commit.hooks = {
     markdownlint.enable = true;
     prettier.enable = true;
