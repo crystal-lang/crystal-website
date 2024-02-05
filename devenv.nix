3d1b46b8bb465491ca78f96b8dd280c83f945281
Johannes Müller <straightshoota@gmail.com>
2024-02-05 18:09:02 +0100

    Set `RUBYOPT` in `devenv.nix`

--- devenv.nix
+++ devenv.nix
@@ -3,6 +3,10 @@
 {
   languages.ruby.enable = true;
 
+  # This is required to fix encoding errors in Jekyll.
+  # `encode': "\\xC3" from ASCII-8BIT to UTF-8 (Encoding::UndefinedConversionError)
+  env.RUBYOPT = "-Eutf-8";
+
   processes.serve.exec = "make serve";
 
   enterShell = ''
