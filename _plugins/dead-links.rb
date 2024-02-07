7d409bc457b6545cc62c4184f23770d0b414921d
Johannes Müller <straightshoota@gmail.com>
2024-02-07 15:50:49 +0100

    Replace dead link by archive linke entirely

--- _plugins/dead-links.rb
+++ _plugins/dead-links.rb
@@ -1,6 +1,6 @@
 Jekyll::Hooks.register :documents, :post_convert do |doc|
   timestamp = doc.date.strftime("%Y%m%d%H%M%S")
   doc.content = doc.content.gsub(%r{<del><a href="([^"]*)">(.*)</a></del>}, <<~HTML)
-    <del><a href="\\1" title="This link appears to be broken" data-dead-link data-proofer-ignore>\\2</a></del><ins>(<a href="https://web.archive.org/web/#{timestamp}/\\1">archive</a>)</ins>
+    <a href="https://web.archive.org/web/#{timestamp}/\\1" title="This link leads to an archived version of the target page which appears to be no longer available" data-dead-link>\\2</a>
     HTML
 end
