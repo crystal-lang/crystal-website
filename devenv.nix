f6e1096e07fee77db5153b01e5f1e3eaa61f7cce
Johannes Müller <straightshoota@gmail.com>
2024-09-19 22:16:10 +0200

    Update Ruby package in devenv: `ruby_3_3` (#838)

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 
 {
-  languages.ruby.enable = true;
+  languages.ruby = {
+    enable = true;
+    package = pkgs.ruby_3_3;
+  };
 
   # This is required to fix encoding errors in Jekyll.
   # `encode': "\\xC3" from ASCII-8BIT to UTF-8 (Encoding::UndefinedConversionError)
