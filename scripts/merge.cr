6cd606f8d28edecdc73369e23508758cb723bc1d
Johannes Müller <straightshoota@gmail.com>
2024-10-10 14:26:10 +0200

    Fix updating `all_time` value for *other* sponsors (#854)
    For sponsors who are not tracked through OpenCollective we need to update the `all_time` value explicitly.
    #710 introduced an automation for this but it was completely flawed and never actually worked as intended (see https://github.com/crystal-lang/crystal-website/pull/851#discussion_r1791570578).
    
    With this patch we keep track of `time_last_payment` in `_data/others.json`. The original value is set to March 2024 when we last updated the current `all_time` values.
    The merge script now automatically adds the amount of the current montly contribution for every month since the last update. Usually the script runs at least once per month, so it should only add one amount. But it also catches up gracefully if there's an interruption (and it works for the initial catch up).
    The script then writes the updated values for `all_time` and `time_last_payment` (set to now) back to `_data/others.json` and commits it so we keep track of which monthly contribution has already been processed.

--- scripts/merge.cr
+++ scripts/merge.cr
@@ -16,8 +16,6 @@ end
 all_sponsors_map = Hash(UInt64, Sponsor).new
 overrides = Array(Sponsor).new
 
-update_other_sponsor_totals = ENV["UPDATE_OTHER_SPONSOR_TOTALS"]? == "1" || Time.utc.day == 1 # only do this on the first day of the month
-
 SPONSOR_DATA = begin
   csv = CSV.new(File.read("#{__DIR__}/../_data/sponsors.csv"), headers: true)
   hash = Hash(UInt64, Int32).new
@@ -28,28 +26,25 @@ SPONSOR_DATA = begin
 end
 
 %w(opencollective.json bountysource.json others.json).each do |filename|
-  File.open("#{__DIR__}/../_data/#{filename}") do |file|
-    sponsors = Array(Sponsor).from_json(file)
+  path = "#{__DIR__}/../_data/#{filename}"
+  sponsors = Array(Sponsor).from_json(File.read(path))
 
-    if filename == "others.json"
-      sponsors, overrides = sponsors.partition(&.overrides.nil?)
-      if update_other_sponsor_totals
-        sponsors.map! do |sponsor|
-          prev_value = SPONSOR_DATA[sponsor.id]?
-          if !prev_value
-            Log.warn { "Can't find sponsor '#{sponsor.name}' in sponsors.csv" }
-            prev_value = 0
-          end
-          sponsor.all_time = prev_value + sponsor.last_payment
-          sponsor
-        end
+  if filename == "others.json"
+    now = Time.utc
+    sponsors.map!(&.update_all_time!(now))
+    File.open(path, "w") do |file|
+      JSON.build(file, indent: 2) do |builder|
+        sponsors.to_json(builder)
       end
+      file.puts # write newline at end of file
     end
 
-    sponsors.each do |sponsor|
-      prev_sponsor = all_sponsors_map[sponsor.id]?
-      all_sponsors_map[sponsor.id] = prev_sponsor ? sponsor.merge(prev_sponsor) : sponsor
-    end
+    sponsors, overrides = sponsors.partition(&.overrides.nil?)
+  end
+
+  sponsors.each do |sponsor|
+    prev_sponsor = all_sponsors_map[sponsor.id]?
+    all_sponsors_map[sponsor.id] = prev_sponsor ? sponsor.merge(prev_sponsor) : sponsor
   end
 end
 
