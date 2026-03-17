5d22dca3ea74cf51a3c69c72121d990d321ab8b0
Johannes Müller <straightshoota@gmail.com>
2026-03-17 20:23:18 +0100

    Use `bundler` in devenv config

--- devenv.nix
+++ devenv.nix
@@ -4,6 +4,7 @@
   languages.ruby = {
     enable = true;
     package = pkgs.ruby_3_3;
+    bundler.enable = true;
   };
 
   # This is required to fix encoding errors in Jekyll.
@@ -22,11 +23,6 @@
   ]);
 
   processes.serve.exec = "make serve";
-
-  enterShell = ''
-    # Automatically run bundler upon enterting the shell.
-    bundle install --quiet
-  '';
 
   git-hooks.hooks = {
     actionlint.enable = true;
